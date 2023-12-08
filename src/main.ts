6233592af2c4f9d56059c129da0010ca30611e6b
唐无忌 <tangwuji20040212@163.com>
2023-12-08 21:38:01 +0800

    完成项目初始化，和elPlus的引入

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
-
+// app.use(router)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 app.mount('#app')
