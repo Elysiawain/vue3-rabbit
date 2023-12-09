ea63f25a6656e2cfb53d2f4e9d4cd7ea8eaf3ebf
唐无忌 <tangwuji20040212@163.com>
2023-12-09 11:19:02 +0800

    完成Layout基础布局

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElPopconfirm: typeof import('element-plus/es')['ElPopconfirm']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import LayoutNav from './LayoutNav.vue';
+import LayoutFooter from './LayoutFooter.vue';
+import LayoutHeader from './LayoutHeader.vue';
 </script>
 
 <template>
-    <div>
-        <el-button type="primary">layout路由</el-button>
-        <el-button type="primary" @click="$router.push('/home')">跳转home路由</el-button>
-        <el-button type="primary" @click="$router.push('/category')">跳转category路由</el-button>
-    </div>
-    <RouterView />
+    <LayoutNav /> 
+    <LayoutHeader /> 
+    <!-- // 二级路由出口 -->
+    <RouterView /> 
+    <LayoutFooter />
 </template>
 
 <style scoped></style>
