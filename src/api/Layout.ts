47e1bf64a2afaa79289c67e50aa51572af9ed46d
唐无忌 <tangwuji20040212@163.com>
2023-12-09 14:09:51 +0800

    利用vueUse插件实现顶部吸附

--- src/api/Layout.ts
+++ src/api/Layout.ts
@@ -1,7 +1,7 @@
 import request from "@/utils/request";
 
-
-export const getCategoryAPI:object = () => {
+// 获取顶部分类导航
+export const getCategoryAPI = () => {
     return request({
         url: "/home/category/head",
         method: "get"
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,9 +3,11 @@ import { ref } from 'vue'
 import LayoutNav from './LayoutNav.vue';
 import LayoutFooter from './LayoutFooter.vue';
 import LayoutHeader from './LayoutHeader.vue';
+import LayoutFlex from './LayoutFlex.vue';
 </script>
 
 <template>
+    <LayoutFlex/>
     <LayoutNav /> 
     <LayoutHeader /> 
     <!-- // 二级路由出口 -->
