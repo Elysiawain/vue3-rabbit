95526fb9c372ef6299636c5893f83fb667f96b8b
唐无忌 <tangwuji20040212@163.com>
2023-12-10 12:32:57 +0800

    完善商品数据加载渲染

--- src/views/Layout/Category/components/SubCategory.vue
+++ src/views/Layout/Category/components/SubCategory.vue
@@ -19,13 +19,34 @@ const goodsData = ref({
     sortField: 'publishTime'
 })
 const getSubCategory = async () => {
-    const { data:{result}} = await getSubCategoryAPI(goodsData.value)
+    const { data: { result } } = await getSubCategoryAPI(goodsData.value)
     goodsList.value = result.items
 }
 onMounted(() => {
-    getCategoryFilter(),
-        getSubCategory()
+    getCategoryFilter()
+    getSubCategory()
 })
+const tabChange = () => {
+    goodsData.value.page = 1
+    getSubCategory()
+}
+// 商品列表无限滚动
+const disabled = ref<boolean>(false)
+const isOver = ref<boolean>(false)
+const loadingMore = async () => {
+    console.log('加载更多')
+    // 1、将当前页数加一重新请求
+    goodsData.value.page++
+    const { data: { result } } = await getSubCategoryAPI(goodsData.value)
+    if (result.items.length === 0) {
+        disabled.value = true
+        isOver.value = true
+        return
+    }
+    // 拼接新老数组
+    goodsList.value = [...goodsList.value, ...result.items]
+}
+
 </script>
 
 <template>
@@ -40,14 +61,18 @@ onMounted(() => {
             </el-breadcrumb>
         </div>
         <div class="sub-container">
-            <el-tabs>
+            <!-- 绑定后对应的tab数据后就可以实现双向数据 -->
+            <el-tabs v-model="goodsData.sortField" @tab-change="tabChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="loadingMore" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                 <GoodsItem v-for="item in goodsList" :key="item.id" :goodDetail="item"></GoodsItem>
+            </div>
+            <div v-if="isOver" class="isOver">
+                已经到底了(●'◡'●)~~~
             </div>
         </div>
     </div>
@@ -108,6 +133,16 @@ onMounted(() => {
         justify-content: center;
     }
 
+    .isOver {
+        display: flex;
+        justify-content: center;
+        aign-items: center;
+        color: #999;
+        font-size: 16px;
+        width: 100%;
+        height: 20px;
+    
+    }
 
 }
 </style>
